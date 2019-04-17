ad6c30fece022b046c4df8830b610165cb456b33
Md. Khalid Rahman <khalid.mr@samsung.com>
2019-04-17 18:19:13 +0600

    droidcafe edited and tabexperiment project added

---
+++ TabExperiment/app/src/main/java/com/example/srbd/tabexperiment/PagerAdapter.java
@@ -0,0 +1,38 @@
+package com.example.srbd.tabexperiment;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by srbd on 4/17/19.
+ */
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int mNumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = mNumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new TabFragment1();
+            case 1:
+                return new TabFragment2();
+            case 2:
+                return new TabFragment3();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
